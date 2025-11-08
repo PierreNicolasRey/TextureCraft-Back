8276ac39a48d8dbdb73700ae9400c2ad5cff2d3f
PierreNicolasRey <pnrey@hotmail.fr>
2025-11-08 11:01:59 +0100

    TC-B: Ajout CORS policy

--- src/main/java/fr/texturecraft/controller/GenererTextureController.java
+++ src/main/java/fr/texturecraft/controller/GenererTextureController.java
@@ -9,12 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/generer")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GenererTextureController {
   private final GenererTextureService genererTextureService;
   private final GenerationFormMapper generationFormMapper;
